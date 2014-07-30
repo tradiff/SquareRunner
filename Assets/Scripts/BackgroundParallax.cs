25a026489569d1da057500351ddc1af2842351b7
TravisTX <travistx@gmail.com>
2014-07-30 07:41:25 -0400

    Started figuring out how to do background parallax

---
+++ Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    public Transform[] Backgrounds;
+    public float ParallaxScale;
+    public float ParallaxReductionFactor;
+    public float Smoothing;
+
+    private Vector3 _lastPosition;
+
+    void Start()
+    {
+        _lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        var parallax = (_lastPosition.x - transform.position.x) * -ParallaxScale;
+
+        for (int i = 0; i < Backgrounds.Length; i++)
+        {
+            var backgroundTargetPosition = Backgrounds[i].position.x + parallax * (i * ParallaxReductionFactor + 1);
+            Backgrounds[i].position = Vector3.Lerp(
+                Backgrounds[i].position, // from
+                new Vector3(backgroundTargetPosition, Backgrounds[i].position.y, Backgrounds[i].position.z), // to
+                Smoothing * Time.deltaTime);
+        }
+        _lastPosition = transform.position;
+    }
+}
